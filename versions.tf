9ff3291c2321bc6a845b89d1e20ced3e9749f11e
CristianR11 <40369712+CristianR11@users.noreply.github.com>
2021-09-16 16:28:37 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "= 1.31"
+      version = "= 1.29"
     }
   }
-  required_version = ">= 0.12"
+  required_version = "= 0.14"
 }
